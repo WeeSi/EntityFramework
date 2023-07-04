19243e844edfa3b6177a9dd4177aeaa270ea9100
WeeSi <francklucasehui@gmail.com>
2023-07-04 14:17:09 +0200

    Create Category ?

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Mvc;
+using PostgreSQL.Data;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+namespace PostGresAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CategoryController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+    private readonly AppDbContext _context;
+
+    private readonly ILogger<CategoryController> _logger;
+
+    public CategoryController(ILogger<CategoryController> logger, AppDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    [HttpGet(Name = "GetCategories")]
+    public IEnumerable<Categories> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new Categories
+        {
+            CategoryName = DateOnly.FromDateTime(DateTime.Now.AddDays(index)).ToString(),
+        })
+        .ToArray();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] Categories model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        // Créer une instance de votre contexte de base de données (DbContext)
+        // Créer une entité Blog à partir du modèle reçu
+        var categoryEntity = new Categories
+        {
+            CategoryName = model.CategoryName,
+        };
+
+        // Ajouter l'entité à votre contexte de base de données
+        _context.Categories.Add(categoryEntity);
+
+        // Enregistrer les modifications dans la base de données
+        await _context.SaveChangesAsync();
+
+        return Ok(model);
+    }
+}
--- models/Categories.cs
+++ models/Categories.cs
@@ -3,12 +3,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace PostgreSQL.Data
 {
-    public class Categories: BaseModel
+    public class Categories : BaseModel
     {
         [Key]
         public int CategoryId { get; set; }
         [Required, MaxLength(40)]
         public string CategoryName { get; set; } = String.Empty;
-        public ICollection<Blogs> Blogs { get; } = new List<Blogs>();
     }
 }
